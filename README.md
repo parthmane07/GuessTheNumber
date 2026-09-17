5a089ae976e631a20d91f76a67b1b6b088ff8c0c
Parth Mane <parthmane333@gmail.com>
2026-09-17 21:00:01 +0530

    Added new program

---
+++ README.md
@@ -0,0 +1,2 @@
+# Guess-The-Number
+Bulding a system that guess the number.
---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+print("Guess a number in your mind")
+input("Press Enter to continue...")
+
+print("Double the number")
+input("Press Enter to continue...")
+
+num = random.randrange(2, 100, 2)
+
+print("Add", num)
+input("Press Enter to continue...")
+
+print("Half the number")
+input("Press Enter to continue...")
+
+print("Remove the number you considereds")
+input("Press Enter to continue...")
+
+print("Your ans is", num/2)
